ed6c68b9364a855c0f3f7d2847298771c313097f
adambor <adamborteam@gmail.com>
2025-10-17 12:47:06 +0200

    Add optional warmup function to messenger

--- dist/messaging/Messenger.d.ts
+++ dist/messaging/Messenger.d.ts
@@ -1,6 +1,7 @@
 import { Message } from "./messages/Message";
 export interface Messenger {
     init(): Promise<void>;
+    warmup?(): Promise<void>;
     broadcast(msg: Message): Promise<void>;
     subscribe(callback: (msg: Message) => void): Promise<void>;
     unsubscribe(callback: (msg: Message) => void): Promise<boolean>;
