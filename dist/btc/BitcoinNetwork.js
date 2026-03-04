0e7a35da45175cdbe03159fbde97b688a9989e11
adambor <adamborteam@gmail.com>
2026-03-04 10:34:30 +0100

    Add alias for TESTNET as TESTNET3

--- dist/btc/BitcoinNetwork.js
+++ dist/btc/BitcoinNetwork.js
@@ -8,8 +8,24 @@ exports.BitcoinNetwork = void 0;
  */
 var BitcoinNetwork;
 (function (BitcoinNetwork) {
+    /**
+     * Bitcoin mainnet network
+     */
     BitcoinNetwork[BitcoinNetwork["MAINNET"] = 0] = "MAINNET";
+    /**
+     * Bitcoin testnet3 network (older test network)
+     */
     BitcoinNetwork[BitcoinNetwork["TESTNET"] = 1] = "TESTNET";
+    /**
+     * Bitcoin testnet3 network (older test network), alias for {@link BitcoinNetwork.TESTNET}
+     */
+    BitcoinNetwork[BitcoinNetwork["TESTNET3"] = 1] = "TESTNET3";
+    /**
+     * Bitcoin testnet4 network (newer test network)
+     */
     BitcoinNetwork[BitcoinNetwork["TESTNET4"] = 2] = "TESTNET4";
+    /**
+     * Bitcoin regtest network, for local regression test modes
+     */
     BitcoinNetwork[BitcoinNetwork["REGTEST"] = 3] = "REGTEST";
 })(BitcoinNetwork = exports.BitcoinNetwork || (exports.BitcoinNetwork = {}));
--- src/btc/BitcoinNetwork.ts
+++ src/btc/BitcoinNetwork.ts
@@ -4,8 +4,24 @@
  * @category Bitcoin
  */
 export enum BitcoinNetwork {
+    /**
+     * Bitcoin mainnet network
+     */
     MAINNET=0,
+    /**
+     * Bitcoin testnet3 network (older test network)
+     */
     TESTNET=1,
+    /**
+     * Bitcoin testnet3 network (older test network), alias for {@link BitcoinNetwork.TESTNET}
+     */
+    TESTNET3=1,
+    /**
+     * Bitcoin testnet4 network (newer test network)
+     */
     TESTNET4=2,
+    /**
+     * Bitcoin regtest network, for local regression test modes
+     */
     REGTEST=3
 }
